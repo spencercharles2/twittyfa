22b2f8d9952c957e0b83bade028d2bbfc0cf48d5
worstplayer32 <worstplayer32@gmail.com>
2020-09-12 16:23:11 +0430

    s

--- test.py
+++ test.py
@@ -7,29 +7,17 @@ auth.set_access_token(twitter_auth.Access_token, twitter_auth.Access_token_secre
 
 api = tweepy.API(auth)
 
-tweets = api.home_timeline(screen_name='PankeSa2', result_type='popular',count=50)
+tweets = api.home_timeline(screen_name='PankeSa2', result_type='popular',count=50,page=10)
 
 with open('output.csv','w',newline='',encoding='utf8') as csvfile:
     fieldnames = ['url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     for tweet in tweets:
-        #writer.writerow({'url':f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}", 
-        #                    'user':'username'})
-        #print(tweet.favorite_count)
-        if tweet.favorite_count > 10:
+        if tweet.favorite_count > 50:
             #writer.writerow({'url':f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"})
             os.system('telegram-send '+f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}")
             os.system('telegram-send ' + str(tweet.favorite_count))
             
 
 
-# csv_writer = csv.writer(open('file.csv','w',encoding='utf-8',newline=''))
-# csv_writer.writerow(['a']['b'])
-# for tweet in tweets:
-#     csv_writer.writerow([f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"])
-#with open('output.csv', 'w', encoding='utf8') as output:
-#f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"+"\n"
-
-
-#{'url':f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"}
